087c7b0e61bd12302ed544b589c05971ec77fc61
Zack Klinger <zklinger@gmail.com>
2016-08-22 17:28:06 -0700

    CREATE updateGrid and getCursorPosition for toggling 'alive' state

--- app.js
+++ app.js
@@ -26,33 +26,74 @@ const initBoard = (width = 300, height = 300, scale = 100) => {
 const initialState = {
   board: initBoard(),
 };
-
+// Update Grid
+const updateGrid = (grid, cellName) => {
+  return _.mapObject(grid, (cell) => {
+    if (grid[cellName] === cell) {
+      console.log('match!');
+      return cell.alive
+        ? Object.assign({}, cell, { alive: 0 })
+        : Object.assign({}, cell, { alive: 1 });
+    }
+    return cell;
+  });
+};
+// Board Reducer
 const boardReducer = (state = initialState.board, action) => {
   switch (action.type) {
+    case 'TOGGLE_CELL':
+      const grid = updateGrid(state.grid, action.cell);
+      return Object.assign({}, state, { grid });
     default:
       return state;
   }
 };
-
+// Root Reducer passed to store
 const rootReducer = combineReducers({
   board: boardReducer,
 });
-
+// Board Component renders the board's current state and adds click handler for cells
 class Board extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      canvas: {},
+      ctx: {}
+    };
+    this.getCursorPosition = this.getCursorPosition.bind(this);
+  }
+
+  getCursorPosition(event, canvas, board) {
+    const { scale } = board;
+    const rect = canvas.getBoundingClientRect();
+    const x = Math.floor((event.clientX - rect.left) / scale);
+    const y = Math.floor((event.clientY - rect.top) / scale);
+
+    return cellName(x, y);
+  }
+
   componentDidMount() {
     const canvas = document.getElementById("myCanvas");
     const ctx = canvas.getContext("2d");
+
+    this.setState({
+      canvas: canvas,
+      ctx: ctx
+    });
   }
 
   render() {
-    const {width, height} = this.props.board;
+    const { board, toggleCell } = this.props;
+    const { canvas, ctx } = this.state;
+
     return (
       <canvas
         id="myCanvas"
-        width={width}
-        height={height}
+        width={board.width}
+        height={board.height}
         style={{backgroundColor: 'black'}}
-        onClick={(event) => console.log('hello')}
+        onClick={(event) => toggleCell(this.getCursorPosition(event, canvas, board))}
       >
         Your browser doesn't support this app.
       </canvas>
@@ -61,21 +102,23 @@ class Board extends Component {
 }
 
 Board.propTypes = {
-  board: PropTypes.object.isRequired
+  board: PropTypes.object.isRequired,
+  toggleCell: PropTypes.func.isRequired
 };
 
 class Game extends Component {
   render() {
-    const { board } = this.props;
+    const { board, toggleCell } = this.props;
 
     return (
-      <Board board={board} />
+      <Board board={board} toggleCell={toggleCell} />
     );
   }
 }
 
 Game.propTypes = {
-  board: PropTypes.object.isRequired
+  board: PropTypes.object.isRequired,
+  toggleCell: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state) => {
@@ -86,7 +129,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onClick: (cell) => {
+    toggleCell: (cell) => {
       dispatch({
         type: 'TOGGLE_CELL',
         cell
